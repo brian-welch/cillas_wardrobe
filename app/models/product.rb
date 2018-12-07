80b2ae1f50a0756e40491ffb44abb422518d61a3
Brian Welch <myself@brianwelch.se>
2018-12-07 13:55:56 +0100

    change migration order to follow dependency logic

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,12 @@ class Product < ApplicationRecord
   belongs_to :main_category
   belongs_to :category
   belongs_to :sub_category
+
+  has_many :materials, through: :product_materials
+  has_many :tags, through: :product_tags
+  has_many :care_instructions, through: :product_care_instructions
+  has_many :tags, through: :product_tags
+  has_many :orders, through: :order_products
+  has_many :orders, through: :order_products
+
 end
