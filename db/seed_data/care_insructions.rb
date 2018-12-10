5a9c347ef5b840b2541f54da09984d43f0c39e26
Brian Welch <myself@brianwelch.se>
2018-12-10 09:00:26 +0100

    add to seeds

---
+++ db/seed_data/care_insructions.rb
@@ -0,0 +1,131 @@
+def care_hash
+  return [
+    {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_1.jpg/$FILE/W_1.jpg",
+      copy: "Wash in commercial machine in water not exceeding 95°C, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_10.jpg/$FILE/W_10.jpg",
+      copy: "Wash in commercial machine in water not exceeding 95°C, at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_2.jpg/$FILE/W_2.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 70°C, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_11.jpg/$FILE/W_11.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 60°C, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_3.jpg/$FILE/W_3.jpg",
+      copy: " Wash in domestic or commercial machine in water not exceeding 60°C, at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_12.jpg/$FILE/W_12.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 50°C, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_4.jpg/$FILE/W_4.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 50°C, at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_13.jpg/$FILE/W_13.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 50°C, at delicate/gentle setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_5.jpg/$FILE/W_5.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 40°C, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_14.jpg/$FILE/W_14.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 40°C, at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_6.jpg/$FILE/W_6.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 40°C, at delicate/gentle setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_15.jpg/$FILE/W_15.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 30°C, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_7.jpg/$FILE/W_7.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 30°C, at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_16.jpg/$FILE/W_16.jpg",
+      copy: "Wash in domestic or commercial machine in water not exceeding 30°C, at delicate/gentle setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_8.jpg/$FILE/W_8.jpg",
+      copy: "Wash gently by hand in water not exceeding 40°C."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_17.jpg/$FILE/W_17.jpg",
+      copy: "Wash gently by hand in water not exceeding 30°C."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_9.jpg/$FILE/W_9.jpg",
+      copy: "Wash in domestic or commercial machine at any temperature, at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/W_18.jpg/$FILE/W_18.jpg",
+      copy: "Do not wash."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/B_1.jpg/$FILE/B_1.jpg",
+      copy: "Use any bleach when needed."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/B_2.jpg/$FILE/B_2.jpg",
+      copy: "Use only non-chlorine bleach when needed."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/B_3.jpg/$FILE/B_3.jpg",
+      copy: "Do not bleach."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_1.jpg/$FILE/D_1.jpg",
+      copy: "Tumble dry at high heat (not exceeding 75°C) at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_8.jpg/$FILE/D_8.jpg",
+      copy: "Tumble dry at medium heat (not exceeding 65°C) at normal setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_2.jpg/$FILE/D_2.jpg",
+      copy: "Tumble dry at medium heat (not exceeding 65°C) at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_9.jpg/$FILE/D_9.jpg",
+      copy: "Tumble dry at low heat (not exceeding 55°C) at permanent press setting."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_3.jpg/$FILE/D_3.jpg",
+      copy: "Tumble dry at a low heat (not exceeding 55°C) at delicate cycle."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_10.jpg/$FILE/D_10.jpg",
+      copy: "Tumble dry any heat."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_4.jpg/$FILE/D_4.jpg",
+      copy: "Tumble dry no heat/air dry."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_11.jpg/$FILE/D_11.jpg",
+      copy: "Do not tumble dry."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_5.jpg/$FILE/D_5.jpg",
+      copy: "After extraction of excess water, line dry/hang to dry."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_12.jpg/$FILE/D_12.jpg",
+      copy: "Hang up the soaking wet article to “drip” dry."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_6.jpg/$FILE/D_6.jpg",
+      copy: "After extraction of excess water, dry the article on a suitable flat surface."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_13.jpg/$FILE/D_13.jpg",
+      copy: "Dry in the shade (symbol added to line dry, drip dry, or dry flat)."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/D_7.jpg/$FILE/D_7.jpg",
+      copy: "Do not dry. To be used with “Do not wash” symbol."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/I_1.jpg/$FILE/I_1.jpg",
+      copy: "Iron with or without steam by hand, or press on commercial equipment, at a high temperature (not exceeding 200°C). Recommended temperature for cotton and linen textiles."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/I_4.jpg/$FILE/I_4.jpg",
+      copy: "Iron with or without steam by hand, or press on commercial equipment, at a medium temperature (not exceeding 150°C). Recommended temperature for polyester, rayon, silk, triacetate and wool textiles."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/I_2.jpg/$FILE/I_2.jpg",
+      copy: "Iron with or without steam by hand, or press on commercial equipment, at a low temperature (not exceeding 110°C). Recommended temperature for acetate, acrylic, modacrylic, nylon, polypropylene and spandex textiles."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/I_5.jpg/$FILE/I_5.jpg",
+      copy: " Do not steam."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/I_3.jpg/$FILE/I_3.jpg",
+      copy: "Do not iron or press."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/P_1.jpg/$FILE/P_1.jpg",
+      copy: "Dry-clean, normal cycle. Any solvent except trichloroethylene"
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/P_2.jpg/$FILE/P_2.jpg",
+      copy: "Dry-clean, normal cycle. Petroleum solvent only."
+    }, {
+      image: "http://www.ic.gc.ca/eic/site/oca-bc.nsf/vwimages/P_3.jpg/$FILE/P_3.jpg",
+      copy: "Do not dry-clean."
+    }
+  ]
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+require 'nokogiri'
+require 'faker'
+require_relative 'seed_data/countries.rb'
+
+role_array = %w[admin customer]
+status_array = %w[pending confirmed processing packed shipped arrived returned]
+style_array = %w[modern classic casual athletic formal]
+segment_array = %w[men women kids home adults girls boys]
+main_category_array = %w[clothes shoes housewares]
+category_array = %w[jackets shirts pants shorts boots trainers flats heels kitchen hall livingroom outdoor bedroom toys dresses skirts decor accessories]
+colors_array = %w[black white grey red orange yellow green blue indego violet multicolored]
+country_array = countries
+
+
